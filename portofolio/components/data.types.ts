41e8b4d34bc0e2818e6e5538119b3269342cd9df
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-29 19:27:46 +0200

    initial NEXT Js portofolio

---
+++ portofolio/components/data.types.ts
@@ -0,0 +1,56 @@
+
+export const SkillsArray :{
+    id: number;
+    courseName : string;
+    percentage : number;
+    width : string;
+    master: boolean;
+    ranking : number;
+}[] = [
+{
+    id: 1,
+    courseName: 'HTML/CSS',
+    percentage: 100,
+    width: "100%",
+    master: true,
+    ranking: 10,
+},
+{
+    id: 2,
+    courseName: 'Java',
+    percentage: 80,
+    width: "80%",
+    ranking: 10,
+    master: true
+},{
+    id: 3,
+    courseName: 'Javascript',
+    percentage: 99,
+    width: "99%",
+    master: true,
+    ranking: 10
+},{
+    id: 4,
+    courseName: 'Node Js/Express Js',
+    percentage: 100,
+    width: "70%",
+    master: true,
+    ranking: 10
+},{
+    id: 5,
+    courseName: 'React Js',
+    percentage: 100,
+    width: "50%",
+    master: true,
+    ranking: 10,
+
+},
+{
+    id: 6,
+    courseName: 'FOP',
+    percentage: 100,
+    width: "40%",
+    master: true,
+    ranking: 10,
+}
+]
