d09c1267b2023f9d48bf4fdedf74b1cb08f9e9df
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-29 19:54:46 +0200

    styling the filter tags

--- portofolio/components/Projects.tsx
+++ portofolio/components/Projects.tsx
@@ -7,7 +7,25 @@ function Projects() {
             <h1 className="font-semibold">PORTOFOLIO</h1>
             <p className="text-white">Since I came into this field, I've gained a good understanding of problem-solving and how to approach challenges posed by different stacks and environments and have dedicated a lot of time to deliver fantastic products at all costs.</p>
         </div>
-        
+        <div className="flex justify-center text-backG gap-4">
+          <ul className="flex gap-4 ">
+            <li>
+              <a href="/">ALL</a>
+            </li>
+            <li>
+              <a href="/ui">UI</a>
+            </li>
+            <li>
+              <a href="/web">Web</a>
+            </li>
+            <li>
+              <a href="/web">Sanity</a>
+            </li>
+            <li>
+              <a href="/others">Others</a>
+            </li>
+          </ul>
+        </div>
         <div>
             
         </div>
