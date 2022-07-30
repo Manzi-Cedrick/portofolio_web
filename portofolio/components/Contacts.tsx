1ac445058c62b863c877d676d083d47e3bb176f3
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-30 20:12:33 +0200

    the contacts page

---
+++ portofolio/components/Contacts.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Contacts() {
+  return (
+    <div className="min-h-screen px-4  md:overflow-hidden lg:px-[8em] text-white  bg-black">
+        <div className="text-center mx-auto flex text-backG text-[14px] flex-col gap-4 justify-center py-5  ">
+            <h1 className="font-semibold">PORTOFOLIO</h1>
+            <p className="text-[#ffffffab]">Since I came into this field, I've gained a good understanding of problem-solving and how to approach challenges posed by different stacks and environments and have dedicated a lot of time to deliver fantastic products at all costs.</p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Contacts
