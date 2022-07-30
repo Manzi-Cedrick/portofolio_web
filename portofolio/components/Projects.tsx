2047d9118e7f84230c74b55dee446fd8cca7d7c5
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-30 11:06:59 +0200

    react icons addition

--- portofolio/components/Projects.tsx
+++ portofolio/components/Projects.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import { ProjectDataArray } from './data.types'
-
+import { FaPlus, FaLink } from 'react-icons/fa';
 function Projects() {
   return (
     <div className="min-h-screen px-4  md:overflow-hidden lg:px-[8em] text-white  bg-black">
@@ -27,17 +27,17 @@ function Projects() {
                 others
             </button>
         </div>
-        <div className='grid grid-cols-3 gap-4'>
+        <div className='grid grid-cols-3 py-6 gap-4'>
           {ProjectDataArray.map((project)=>(
             <div key={project.projectId} className='group bg-opacity-80 min-h-[30vh] min-w-[20vw] relative overflow-hidden'>
-                <img src={`${project.projectImage}`} className='object-cover group-hover:scale  ' alt="" />
+                <img src={`${project.projectImage}`} className='object-cover group-hover:scale-100  ' alt="" />
                 <div className='text-white group-hover:flex bg-slate-900 w-full h-full bg-opacity-80 absolute top-0 left-0 flex-col hidden duration-500 justify-center place-items-center'>
-                    <h1>{project.projectName}</h1>
+                    <h1 className='text-3xl font-bold text-slate-400'>{project.projectName}</h1>
                     <p>{project.projectCourses}</p>
                     <small>{project.tags.map((tageach)=>(tageach))}</small>
-                    <div className='flex '>
-                      <button className="btn text-white btn-primary  p-5 lg:px-10">View</button>
-                      <Link href={`${project.projectUrl}`} className="btn text-white btn-primary  p-5 lg:px-10">Link</Link>
+                    <div className='flex gap-4'>
+                      <button className="btn text-white bg-backG rounded-full btn-primary  p-3 "> <FaPlus/> </button>
+                      <button className="btn text-white bg-backG rounded-full btn-primary  p-3 "><FaLink/></button>
                     </div>
                 </div>
             </div>
