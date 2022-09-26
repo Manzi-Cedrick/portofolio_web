797142fd51c06d7b9dfbe7048e6fade310d6902e
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-26 08:14:01 +0200

    Nprogress Configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Nprogress from  'nprogress';
+import { Router } from 'next/router';
+
+Nprogress.configure({ showSpinner: false });
+
+Router.events.on("routeChangeStart", () => Nprogress.start());
+Router.events.on("routeChangeComplete", () => Nprogress.done());
+Router.events.on("routeChangeError", () => Nprogress.done());
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
