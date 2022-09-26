28a268ff53147293ee4897d91e64dfb7cb266891
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-26 08:14:56 +0200

    LinkArr Initialisation

---
+++ utils/constants.tsx
@@ -0,0 +1,21 @@
+export const LinkArr :{
+    id:number;
+    linkName : string;
+    linkUrl :string;
+}[]=[{
+    id:1,
+    linkName:'Home',
+    linkUrl:'/#hero'
+},{
+    id:2,
+    linkName:'Skills',
+    linkUrl:'/#skills'
+},{
+    id:3,
+    linkName:'Projects',
+    linkUrl:'/#project'
+},{
+    id:1,
+    linkName:'Contact',
+    linkUrl:'/#contact'
+}]
