70243b5c8787d5c172b70996848ca652d7ca6312
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-26 15:17:54 +0200

    LinkName Styles Update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -15,9 +15,9 @@ function Projects() {
 
     return (
         <div className="min-h-screen px-4  md:overflow-hidden lg:px-[8em] text-white  bg-black" id="project">
-            <div className="text-center mx-auto flex text-backG text-[14px] flex-col gap-4 justify-center py-5  ">
-                <h1 className="font-semibold">PORTOFOLIO</h1>
-                <p className="text-[#ffffffab]">Since I came into this field, I've gained a good understanding of problem-solving and how to approach challenges posed by different stacks and environments and have dedicated a lot of time to deliver fantastic products at all costs.</p>
+             <div className="text-center text-white text-[14px] flex flex-col justify-center place-items-center py-10  ">
+                <h1 className="font-semibold  bg-backG px-4 py-4 text-[10px] animate-pulse">Portofolio</h1>
+                <p className="text-[#ffffffab] py-4">Since I came into this field, I've gained a good understanding of problem-solving and how to approach challenges posed by different stacks and environments and have dedicated a lot of time to deliver fantastic products at all costs.</p>
             </div>
             <div className="flex justify-center text-backG gap-4">
                 <button onClick={() => setProjectData(Project_arr)} className={`btn text-white rounded-full btn-primary bg-backG p-5 lg:px-10`}>
