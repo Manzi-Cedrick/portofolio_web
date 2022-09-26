a131a0b49314df58876d7fdd2be0642924ac4b1e
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-26 14:43:41 +0200

    fix:Nprogress package manage issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Nprogress from  'nprogress';
+import type { AppProps } from 'next/app';
 import { Router } from 'next/router';
+import Nprogress from 'nprogress'
 
-Nprogress.configure({ showSpinner: false });
+Nprogress.configure({ showSpinner: true });
 
 Router.events.on("routeChangeStart", () => Nprogress.start());
 Router.events.on("routeChangeComplete", () => Nprogress.done());
