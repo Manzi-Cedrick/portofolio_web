22f358d35025f2ce0085cb3358be968bde7e5e9c
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-28 13:12:42 +0200

    chore: Session Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import { Router } from 'next/router';
 import Nprogress from 'nprogress'
-
+import { SessionProvider } from 'next-auth/react';
+import Navbar from '../components/Navbar';
 Nprogress.configure({ showSpinner: true });
 
 Router.events.on("routeChangeStart", () => Nprogress.start());
@@ -10,8 +11,13 @@ Router.events.on("routeChangeComplete", () => Nprogress.done());
 Router.events.on("routeChangeError", () => Nprogress.done());
 
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps:{session,...pageProps} }: AppProps) {
+  return (
+      <SessionProvider session={session}>
+        <Navbar/>
+        <Component {...pageProps} />
+      </SessionProvider>
+  );
 }
 
 export default MyApp
