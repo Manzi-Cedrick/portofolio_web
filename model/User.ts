de1a140d95d0b9e5f2b8c02361c9457325c38f2a
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-28 13:11:12 +0200

    chore: user model

---
+++ model/User.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+export default User;
