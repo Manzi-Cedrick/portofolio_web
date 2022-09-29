052afa0ed37cd0e8eea5d2789409945fa01573bd
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-29 18:53:30 +0200

    chore : styles update

--- components/auth/SignInForm.tsx
+++ components/auth/SignInForm.tsx
@@ -1,11 +1,20 @@
 import { NextPage } from 'next'
-import React from 'react'
-import { FaGithub, FaGithubAlt } from 'react-icons/fa'
+import { signIn } from 'next-auth/react'
+import React, { useState } from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { LoginFormDummy, LoginStructure } from '../../utils/data.types'
 
 const SignInForm = ({GithubSignIn}:{GithubSignIn:any}) => {
+    const [FormData,setFormData] = useState<LoginStructure>(LoginFormDummy);
     const handleSignOut = () =>{
         GithubSignIn();
     }
+    const handleSubmit = async (e:React.FormEvent<HTMLInputElement>) =>{
+        e.preventDefault();
+        signIn('credentials', { redirect: false, email: FormData?.email, password: FormData?.password , callback : `${window.location.origin}`});
+        
+    }
+    
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -18,14 +27,14 @@ const SignInForm = ({GithubSignIn}:{GithubSignIn:any}) => {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form className="space-y-4 md:space-y-6" method='post' onSubmit={handleSubmit} >
                   <div>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true}/>
+                      <input value={FormData?.email} onChange={(e)=>setFormData({...FormData,email:e.target.value})} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true}/>
                   </div>
                   <div>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true}/>
+                      <input value={FormData?.password} onChange={(e)=>setFormData({...FormData,password:e.target.value})} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true}/>
                   </div>
                   <div className="flex items-center justify-between">
                       <div className="flex items-start">
@@ -39,7 +48,7 @@ const SignInForm = ({GithubSignIn}:{GithubSignIn:any}) => {
                       <a href="#" className="text-sm font-medium text-backG hover:underline ">Forgot password?</a>
                   </div>
                   <button type="submit" className="w-full text-white bg-backG hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <button type="submit" className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex place-items-center justify-center gap-6" onClick={handleSignOut}><FaGithub/> Sign In with Github </button>
+                  <button className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex place-items-center justify-center gap-6" onClick={handleSignOut}><FaGithub/> Sign In with Github </button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                   </p>
