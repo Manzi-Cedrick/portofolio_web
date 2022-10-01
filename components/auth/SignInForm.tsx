c617a700fd5e648994a4024756dfd94e3322ad94
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-01 23:18:56 +0200

    Form Styles update

--- components/auth/SignInForm.tsx
+++ components/auth/SignInForm.tsx
@@ -17,7 +17,7 @@ const SignInForm = ({GithubSignIn}:{GithubSignIn:any}) => {
     
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-[90vh] lg:py-0">
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img className="w-8 h-8 mr-2" src="/assets/logo.svg" alt="logo"/>
           Cedrick Portfolio
