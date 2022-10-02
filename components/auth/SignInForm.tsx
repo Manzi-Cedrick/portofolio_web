ae18ede9cfa97a9e7556dc861b761c512fddea58
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-02 09:16:47 +0200

    handlesubmit functionality reference

--- components/auth/SignInForm.tsx
+++ components/auth/SignInForm.tsx
@@ -27,7 +27,7 @@ const SignInForm = ({GithubSignIn}:{GithubSignIn:any}) => {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6" method='post' onSubmit={handleSubmit} >
+              <form className="space-y-4 md:space-y-6" method='post' onSubmit={(e)=>handleSubmit} >
                   <div>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                       <input value={FormData?.email} onChange={(e)=>setFormData({...FormData,email:e.target.value})} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true}/>
