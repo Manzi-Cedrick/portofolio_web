97128dc45bf8273e9933afed8ec2ec2a4edd2245
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-03 12:08:58 +0200

    Contact Page Administration

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -3,11 +3,11 @@ import { FaInbox, FaLocationArrow, FaPaperPlane, FaPhone } from 'react-icons/fa'
 
 function Contacts() {
   return (
-    <div className="min-h-screen px-4  md:overflow-hidden lg:px-[8em] text-white  bg-black" id="contact">
-        <div className="text-center mx-auto flex text-backG text-[14px] flex-col gap-4 justify-center py-5  ">
-            <h1 className="font-semibold">Contact Page</h1>
+    <div className="min-h-screen px-4  md:overflow-hidden lg:px-[8em] text-white  bg-[#121212]" id="contact">
+<div className="text-center text-white text-[14px] flex flex-col justify-center place-items-center gap-4 py-10  ">
+                <h1 className="font-semibold  bg-backG px-4 py-4 text-[10px] animate-pulse">Contact</h1>
             <p className="text-[#ffffffab]">Since I came into this field, I've gained a good understanding of problem-solving and how to approach challenges posed by different stacks and environments and have dedicated a lot of time to deliver fantastic products at all costs.</p>
-        </div>
+            </div>
         <div className='md:flex flex-col gap-4 relative md:flex-row lg:flex-row justify-between py-10'>
             <div className='flex flex-col gap-8'>
             <div className='flex gap-4'>
