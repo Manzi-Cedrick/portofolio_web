e423f01c093e54e8133c16edfb452d82da130978
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-03 13:34:37 +0200

    Credentials provider addition

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,21 +1,70 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
+import User from "../../../model/User";
+import { compare } from "bcrypt";
 import dbConnect from "../../../lib/dbconnect";
-import GithubProvider from "next-auth/providers/github"
-import clientPromise from "../../../lib/mongodb"
-export const authOptions = {
-  // Configure one or more authentication providers
+
+export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
+    // Email & Password
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        await dbConnect();
+
+        // Find user with the email
+        const user = await User.findOne({
+          email: credentials?.email,
+        });
+
+        // Email Not found
+        if (!user) {
+          throw new Error("Email is not registered");
+        }
+
+        // Check hased password with DB hashed password
+        const isPasswordCorrect = await compare(
+          credentials!.password,
+          user.hashedPassword
+        );
+
+        // Incorrect password
+        if (!isPasswordCorrect) {
+          throw new Error("Password is incorrect");
+        }
+
+        return user;
+      },
+    }),
   ],
-  pages:{
-    signIn:'/auth'
+  pages: {
+    signIn: "/login",
   },
-  debug: process.env.NODE_ENV === 'development',
-  adapter : MongoDBAdapter(clientPromise)
-}
-export default NextAuth(authOptions)
+  debug: process.env.NODE_ENV === "development",
+  adapter: MongoDBAdapter(clientPromise),
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
